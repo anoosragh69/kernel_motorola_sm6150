cb78280814aa4739daebb7576b6729cff5bcf20a
Haijian Ma <mahj8@motorola.com>
2018-09-19 16:59:09 +0800

    power: smb5: Add Factory Commands.
    Add the Factory commands and the lockout for Factory mode.
    
    refer to https://gerrit.mot.com/920848
    
    Change-Id: I8e18ba97d12c734251ea75cb4cac27d0e02cc8e3
    Signed-off-by: Haijian Ma <mahj8@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1245606
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Submit-Approved: Jira Key
    Tested-by: Jira Key
    Reviewed-by: Master Yoda
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>

--- drivers/power/supply/qcom/smb5-lib.h
+++ drivers/power/supply/qcom/smb5-lib.h
@@ -385,6 +385,11 @@ struct smb_iio {
 	struct iio_channel	*smb_temp_chan;
 };
 
+struct mmi_params {
+	bool			factory_mode;
+	bool			demo_mode;
+};
+
 struct smb_charger {
 	struct device		*dev;
 	char			*name;
@@ -617,6 +622,7 @@ struct smb_charger {
 	int			dcin_uv_count;
 	ktime_t			dcin_uv_last_time;
 	int			last_wls_vout;
+	int			wireless_vout;
 	/* GPIO DCIN Supply */
 	int			micro_usb_gpio;
 	int			micro_usb_irq;
@@ -627,6 +633,10 @@ struct smb_charger {
 	int			micro_usb_pre_state;
 	bool			dcin_uusb_over_gpio_en;
 	bool			aicl_disable;
+
+	/* mmi based params */
+	/* Place at end of struct smb_charger as it grows */
+	struct mmi_params	mmi;
 };
 
 int smblib_read(struct smb_charger *chg, u16 addr, u8 *val);
@@ -840,4 +850,7 @@ int smblib_get_qc3_main_icl_offset(struct smb_charger *chg, int *offset_ua);
 
 int smblib_init(struct smb_charger *chg);
 int smblib_deinit(struct smb_charger *chg);
+
+void mmi_init(struct smb_charger *chg);
+void mmi_deinit(struct smb_charger *chg);
 #endif /* __SMB5_CHARGER_H */
